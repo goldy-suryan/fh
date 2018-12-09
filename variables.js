c3d0026bd8c4e90f922e389b2250eae71d12224a
goldy <goldysuryan87@gmail.com>
2018-12-10 02:34:04 +0530

    packages added

--- variables.js
+++ variables.js
@@ -22,20 +22,78 @@ const intro = [
   }
 ];
 const cta = [
-  { location: "Jim Corbett Park" },
-  { location: "Kashmir" },
-  { location: "Goa" }
+  {
+    location: "Jim Corbett Park",
+    text:
+      "Jim Corbett National Park is the oldest national park in India and was established in 1936 as Hailey National Park. The park has sub-Himalayan belt geographical and ecological characteristics. An ecotourism destination, it contains 488 different species of plants and a diverse variety of fauna"
+  },
+  {
+    location: "Kashmir",
+    text:
+      "Kashmir is a multi-faceted diamond, changing its hues with the seasons - always extravagantly beautiful. Two major Himalayan ranges, the Great Himalayan Range and the Pir Panjal, surround the landscape from the north and south respectively. They are the source of great rivers, which flow down into the valleys, forested with orchards and decorated by lily-laden lakes."
+  },
+  {
+    location: "Goa",
+    text:
+      "Goa, the smallest Indian state and much-loved tourist place of India, is situated in the coastal Konkan region and on the bank of Arabian Sea which forms its western coast. Beaches in Goa are the best part of the excellence list of the beach state, where you can rejuvenate yourself."
+  }
 ];
 const indexOffers = [
   {
     url: "golden",
     price: "19999/-",
     title: "Delhi-Agra-Jaipur",
-    person: "couple"
+    person: "couple",
+    list: [
+      "3 Nights Stay In 3 Star Property",
+      "Ac Cabs For Full Trip + Sight Seeing ( Delhi - Agra - Jaipur -Delhi)",
+      "1 Night In Delhi",
+      "1 Night In Agra",
+      "1 Night In Jaipur",
+      "3 Breakfast"
+    ]
   },
-  { url: "nanital", price: "7999/-", title: "Nanital", person: "person" },
-  { url: "manail", price: "6999/-", title: "Manali", person: "person" },
-  { url: "goa", price: "13999/-", title: "Goa", person: "person" }
+  {
+    url: "nanital",
+    price: "7999/-",
+    title: "Nanital",
+    person: "person",
+    list: [
+      "2 Nights Stay In 3 Star Hotel",
+      "Volvo Ac Bus Transfers",
+      "2 Breakfast + 2 Dinner",
+      "2 Nightstay In Nanital",
+      "Nanital Lake",
+      "Naina Devi Temple"
+    ]
+  },
+  {
+    url: "manail",
+    price: "6999/-",
+    title: "Manali",
+    person: "person",
+    list: [
+      "03 Nights 04 Days",
+      "Delhi to Delhi Volvo ticket",
+      "3 Nights stay Manali",
+      "Breakfast and Dinner",
+      "Bus stand pick Up & Drop",
+      "All Sightseeing"
+    ]
+  },
+  {
+    url: "goa",
+    price: "13999/-",
+    title: "Goa",
+    person: "person",
+    list: [
+      "3 Nights Stay In 3 Star Property",
+      "Welcome Soft Drink On Arrival",
+      "Return Air Ticket Delhi - Goa - Delhi",
+      "Well Accommodation Ac Rooms With Pool View Facing",
+      "03 Breakfast 03 Dinner"
+    ]
+  }
 ];
 const offers = [
   {
@@ -124,6 +182,36 @@ const offers = [
       "2 Break Fast, 2 Lunch, 2 Dinner",
       "One Day Jeep Safari (Morning Or Evening)"
     ]
+  },
+  {
+    url: "rishikesh",
+    price: "1400/-",
+    title: "Rishikesh",
+    person: "person",
+    rating: "9",
+    list: [
+      "1 Night 2 Days",
+      "Stay in camp",
+      "Breakfast, Evening Snack & Dinner",
+      "Bonfire",
+      "Music",
+      "16Km River Rafting"
+    ]
+  },
+  {
+    url: "meclodganj",
+    price: "7230/-",
+    title: "Meclodganj",
+    person: "person",
+    rating: "9.2",
+    list: [
+      "2 Nights & 3 Days",
+      "2 Nights stay in Hotel",
+      "Delhi to Delhi volvo ticket",
+      "Breakfast and Dinner",
+      "All Sightseeing",
+      "Bus stand pickup and drop"
+    ]
   }
 ];
 const testimonials = [
@@ -177,6 +265,12 @@ const gallery = [
   },
   {
     url: "ladakh"
+  },
+  {
+    url: "rishikesh"
+  },
+  {
+    url: "meclodganj"
   }
 ];
 module.exports = { intro, cta, offers, indexOffers, testimonials, gallery };
